177eb387eb78b9c295b641019ec10c75f508ae97
mlakani <138949303+mlakani@users.noreply.github.com>
2026-07-15 10:47:39 -0700

    Add current songs to catalog

--- songs.js
+++ songs.js
@@ -1,1 +1,102 @@
+"use strict";
 
+/*
+  MINOO LAKANI MUSIC CATALOG
+
+  For every future release:
+  1. Upload the cover into the images folder.
+  2. Copy one song entry.
+  3. Add the new entry at the TOP of this list.
+*/
+
+window.songCatalog = [
+  {
+    id: "my-own-heart",
+    title: "My Own Heart",
+    alternateTitle: "",
+    image: "images/my-own-heart.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/my-own-heart-2?ref=release",
+    language: "English",
+    featured: true
+  },
+
+  {
+    id: "tell-me-you-love-me",
+    title: "Tell Me You Love Me",
+    alternateTitle: "",
+    image: "images/tell-me-you-love-me.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/tell-me-you-love-me-2?ref=release",
+    language: "English",
+    featured: true
+  },
+
+  {
+    id: "eshgh-o-afsaneh",
+    title: "عشق و افسانه",
+    alternateTitle: "Eshgh O Afsaneh",
+    image: "images/eshgh-o-afsaneh.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/---3?ref=release",
+    language: "Persian",
+    featured: true
+  },
+
+  {
+    id: "begoo-mano-doost-dari",
+    title: "بگو منو دوستم داری",
+    alternateTitle: "Begoo Mano Doost Dari",
+    image: "images/begoo-mano-doost-dari.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/----2?ref=release",
+    language: "Persian",
+    featured: true
+  },
+
+  {
+    id: "hese-naab",
+    title: "حسِ ناب",
+    alternateTitle: "Hese Naab",
+    image: "images/hese-naab.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/--11?ref=release",
+    language: "Persian",
+    featured: true
+  },
+
+  {
+    id: "rade-paye-to",
+    title: "ردِ پای تو",
+    alternateTitle: "Rade Paye To",
+    image: "images/rade-paye-to.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/---2?ref=release",
+    language: "Persian",
+    featured: false
+  },
+
+  {
+    id: "ghashangtarin-dalil",
+    title: "قشنگترین دلیل",
+    alternateTitle: "Ghashangtarin Dalil",
+    image: "images/ghashangtarin-dalil.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/--10?ref=release",
+    language: "Persian",
+    featured: false
+  },
+
+  {
+    id: "khaterate-to",
+    title: "خاطرات تو",
+    alternateTitle: "Khaterate To",
+    image: "images/khaterate-to.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/--9?ref=release",
+    language: "Persian",
+    featured: false
+  },
+
+  {
+    id: "harfe-negah",
+    title: "حرف نگاه",
+    alternateTitle: "Harfe Negah",
+    image: "images/harfe-negah.jpg",
+    link: "https://distrokid.com/hyperfollow/minoolakani/--7?ref=release",
+    language: "Persian",
+    featured: false
+  }
+];
